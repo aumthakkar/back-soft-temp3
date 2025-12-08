1145218c30da5409decd13b540f111821c0ab8f3
aumthakkar <aumthakkar@yahoo.co.uk>
2025-12-08 19:45:08 +0000

    added python-app s/w template

---
+++ python-app/template/src/3py-app.py
@@ -0,0 +1,31 @@
+
+from flask import Flask, jsonify
+import socket
+import time
+
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+# ‘/’ URL is bound with hello_world() function.
+def info():
+    return jsonify({
+        "hostname" : socket.gethostname(),
+        "time" : time.ctime(),
+        "app_name" : "${{values.app_name}}",
+        "env": "${{values.app_env}}",
+        "deployed_on" : "kubernetes",
+        "msg" : "Hello, this is Pranav!"
+    })
+
+@app.route('/api/v1/healthz')
+# ‘/’ URL is bound with hello_world() function.
+def healthz():
+    return jsonify({
+        "status" : "up"
+    })
+
+# main driver function
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0")
